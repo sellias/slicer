165a7390ca3b8db3817c414613da19f4eff83f16
Stephen Ellias <sellias13@gmail.com>
2021-03-05 10:46:54 -0500

    modify demo to use functions

--- demo-1.py
+++ demo-1.py
@@ -9,29 +9,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d as a3
 
-# prepare figure size, add 3D axes, set viewing distance, set limits, set labels
-fig = plt.figure(figsize=(6,6))
-ax = fig.add_subplot(111, projection='3d')
-ax.set_xlim([0,5])
-ax.set_ylim([0,5])
-ax.set_zlim([0,5])
+def main():
+    plot_init()
+    plot_demo_one()
+    plot_show_still()
 
-# prepare a demo triangle, cyan body, black edge, add to axes
-vtx = [2,2,0], [2,1,3], [4,4,5]
-tri = a3.art3d.Poly3DCollection([vtx])
-tri.set_color('c')
-tri.set_edgecolor('k')
-ax.add_collection3d(tri)
+def plot_init():
+    """prepare figure, 3D axes, size, range, viewing distance, labels"""
+    global fig, ax, plot_view_dist
+    fig = plt.figure(figsize=(6,6))
+    ax = fig.add_subplot(111, projection='3d')
+    plot_view_dist = 9
+    ax.set_xlim([0,5])
+    ax.set_ylim([0,5])
+    ax.set_zlim([0,5])
+    plt.ion()
+    return()
 
-# use interactive mode
-plt.ion()
-plt.draw()
+def plot_demo_one():
+    """prepare a demo triangle, plot on axes, draw plot"""
+    vtx = [2,2,0], [2,1,3], [4,4,5]
+    tri = a3.art3d.Poly3DCollection([vtx])
+    tri.set_color('c')
+    tri.set_edgecolor('k')
+    ax.add_collection3d(tri)
 
-# display rotating view forever
-while True:
-    for angle in range(360):
-        # set view angle, view distance, show, and pause
-        ax.view_init(20, angle)
-        ax.dist = 9
-        plt.draw()
-        plt.pause(.001)
+def plot_show_still():
+    plt.show(block=True)
+
+def plot_show_rotating():
+    """rotate a 3D plot, set range, speed multiplyer"""
+    while True:
+        for angle in range(360):
+            # set view angle, view distance, show, and pause
+            ax.view_init(20, angle)
+            ax.dist = plot_view_dist
+            plt.draw()
+            plt.pause(.001)
+
+if __name__ == '__main__':
+    main()
