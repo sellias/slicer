849f87bfd7fb3daac8b877451d40cecf41cc2f8c
Stephen Ellias <sellias13@gmail.com>
2021-03-06 00:05:42 -0500

    update plot styling

--- demo-1.py
+++ demo-1.py
@@ -12,12 +12,12 @@ import mpl_toolkits.mplot3d as a3
 def main():
     plot_init()
     plot_demo_one()
-    plot_show_still()
+    plot_show_rotating()
 
 def plot_init():
     """prepare figure, 3D axes, size, range, viewing distance, labels"""
     global fig, ax, plot_view_dist
-    fig = plt.figure(figsize=(6,6))
+    fig = plt.figure(figsize=(12,12))
     ax = fig.add_subplot(111, projection='3d')
     plot_view_dist = 9
     ax.set_xlim([0,5])
@@ -27,12 +27,26 @@ def plot_init():
     return()
 
 def plot_demo_one():
-    """prepare a demo triangle, plot on axes, draw plot"""
-    vtx = [2,2,0], [2,1,3], [4,4,5]
-    tri = a3.art3d.Poly3DCollection([vtx])
-    tri.set_color('c')
-    tri.set_edgecolor('k')
+    """prepare a demo triangle, demo plane, plot on axes, draw plot"""
+    # segment
+    seg_vtx = [3,3,2.5], [2,1.2,2.5]
+    seg = a3.art3d.Line3DCollection([seg_vtx])
+    seg.set_edgecolor('black')
+    seg.set_linewidth(4)
+    ax.add_collection3d(seg)
+    # triangle
+    tri_vtx = [2,2,0], [2,1,3], [4,4,5]
+    tri = a3.art3d.Poly3DCollection([tri_vtx])
+    tri = a3.art3d.Poly3DCollection([tri_vtx])
+    tri.set_edgecolor('blue')
+    tri.set_color((0, 0.5, 1, 0.4))
+    tri.set_linewidth(2)
     ax.add_collection3d(tri)
+    # plane
+    plane_vtx = [0,0,2.5], [0,5,2.5], [5,5,2.5], [5,0,2.5]
+    plane = a3.art3d.Poly3DCollection([plane_vtx])
+    plane.set_color((1, 0, 0, 0.1))
+    ax.add_collection3d(plane)
 
 def plot_show_still():
     plt.show(block=True)
@@ -42,7 +56,7 @@ def plot_show_rotating():
     while True:
         for angle in range(360):
             # set view angle, view distance, show, and pause
-            ax.view_init(20, angle)
+            ax.view_init(15, angle)
             ax.dist = plot_view_dist
             plt.draw()
             plt.pause(.001)
