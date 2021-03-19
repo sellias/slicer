f29bab02b50052a150a1e1fd106e51ca7c441739
Stephen Ellias <sellias13@gmail.com>
2021-03-19 17:53:10 -0400

    created STLGeometry.py, modified demos

---
+++ STLGeometry.py
@@ -0,0 +1,77 @@
+#!/bin/python
+# ----------------------------------------------------------------
+#   geometry class file
+#       > import stl file
+#       > get statistics on geometry
+#       > return vectors for processing  
+#   
+#   (c) 2021 stephen ellias, new york, usa
+#   released under gnu public license (gpl)
+#   refer to github.com/sellias
+# ----------------------------------------------------------------
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+from stl import mesh
+import numpy as np
+import sys
+
+class STLGeometry:
+    #-----------------------------------------
+    #       for the handling of stl geometry
+    #       importing, parsing, basic stats
+    #       returns list of triangles as three
+    #       sets of 3d vectors
+    #-----------------------------------------
+    """"""
+    VERBOSE = True
+    mesh = None
+    """derived stats"""
+    dimensions = None
+    range = None
+    faces = None
+    x_min = None
+    x_max = None
+    y_min = None
+    y_max = None
+    z_min = None
+    z_max = None
+
+    def __init__(self, stl_filepath):
+        """   import object from stl file   """
+        if self.VERBOSE : print("Importing STL File:" ,stl_filepath)
+        try:
+            self.mesh = mesh.Mesh.from_file(stl_filepath)
+        except FileNotFoundError:
+            sys.exit("ERROR: File Not Found:" ,stl_filepath)
+            
+        """   process derived stats   """
+        self.x_max = np.max(self.mesh.vectors[:,:,0])
+        self.x_min = np.min(self.mesh.vectors[:,:,0])
+        self.y_max = np.max(self.mesh.vectors[:,:,1])
+        self.y_min = np.min(self.mesh.vectors[:,:,1])
+        self.z_max = np.max(self.mesh.vectors[:,:,2])
+        self.z_min = np.min(self.mesh.vectors[:,:,2])
+        self.dimensions = [self.x_max-self.x_min, self.y_max-self.y_min, self.z_max-self.z_min]
+        self.faces = self.mesh.vectors.size
+
+    def get_boundaries(self):
+        return self.x_min, self.x_max, self.y_min, self.y_max, self.z_min, self.z_max
+
+    def print_stats(self):
+        """size, triangle count, file name, range"""
+        print(f'Geometry Statistics:')
+        print(f'\tSize:   \t {self.dimensions[0]:.0f}mm x {self.dimensions[1]:.0f}mm x {self.dimensions[2]:.0f}mm')
+        print(f'\tFace Count:\t {self.faces:,}')
+        print(f'\tX Range:\t {self.x_min:.3f}, {self.x_max:.3f}')
+        print(f'\tY Range:\t {self.y_min:.3f}, {self.y_max:.3f}')
+        print(f'\tZ Range:\t {self.z_min:.3f}, {self.z_max:.3f}')
+
+def main():
+    """import stl file"""
+    geometry = STLGeometry("/home/stephen/projects/slicer/3DBenchy.stl")
+    """display stats"""
+    geometry.print_stats()
+
+if __name__ == '__main__':
+    main()
--- demo-2.py
+++ demo-2.py
@@ -1,28 +1,20 @@
 #!/bin/python
 # ----------------------------------------------------------------
+#   demo 2
+#       > import stl file
+#       > visualize data  
 #   
-#   
-#   (C) 2021 Stephen Ellias, New York, USA
-#   Released under GNU Public License (GPL)
-#   Refer to github.com/sellias
+#   (c) 2021 stephen ellias, new york, usa
+#   released under gnu public license (gpl)
+#   refer to github.com/sellias
 # ----------------------------------------------------------------
 
-import matplotlib.pyplot as plt
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+from stl import mesh
 
-class Plotter:
-    """A sample example plotter"""
+def main():
+    print("demo 3...")
 
-    def __init__(self):
-        self.fig = plt.figure(figsize=(6,6))
-        self.ax = self.fig.add_subplot(111, projection='3d')
-        self.view_dist = 9
-        self.xlim = 5
-        self.ylim = 5
-        self.zlim = 5
-    
-    def set_limits(self, ixlim, iylim, izlim):
-        self.ax.set_xlim(ixlim)
-        self.ax.set_ylim(iylim)
-        self.ax.set_zlim(izlim)
-
-
+if __name__ == '__main__':
+    main()
